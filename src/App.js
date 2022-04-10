aeb7b1259e51e55871a7e91c4b051540b09911b7
Djordje Knezevic <starwarrior@mycity.rs>
2022-04-10 15:44:53 +0200

    Test child components re-evaluation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Button from './components/UI/Button/Button';
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -15,7 +16,8 @@ function App() {
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput />
+      {/* <DemoOutput show={showParagraph} /> */}
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
